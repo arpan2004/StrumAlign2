218ccc1676a0e8666b5e1d1fb8d4027914ce1568
arpan2004 <arpan2004.AD@gmail.com>
2024-10-17 14:52:35 -0400

    cleanup

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo2.png";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-[#6a9c78] text-white py-2 px-5">
+      <div className="flex justify-between items-center">
+        <Link to="/" className="text-white text-xl">
+          <img src={logo} alt="logo" className="w-14 h-auto" />
+        </Link>
+        <div className="flex space-x-5">
+          <Link to="/" className="text-white text-xl hover:underline">
+            Home
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- client/src/pages/Record.jsx
+++ client/src/pages/Record.jsx
@@ -1,18 +1,35 @@
-const Record = () => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-customBackground p-5">
-      <h1 className="font-sans text-2xl text-gray-800 mb-5 text-center">
-        Real-Time Hand Tracking
-      </h1>
-      <div className="flex justify-center items-center w-full max-w-2xl rounded-lg overflow-hidden shadow-md bg-white">
-        <img
-          src="http://localhost:5000/video_feed"
-          alt="Hand Tracking"
-          className="w-full h-auto object-cover"
-        />
-      </div>
-    </div>
-  )
-}
+import { useEffect } from "react";
+import Navbar from "../components/Navbar";
 
-export default Record
+const Record = () => {
+  useEffect(() => {
+    const videoElement = document.getElementById("video-feed");
+    if (videoElement) {
+      videoElement.src = "http://localhost:5000/video_feed";
+    }
+
+    return () => {
+      videoElement.src = "";
+    };
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center min-h-screen bg-customBackground p-5">
+        <h1 className="font-sans text-2xl text-gray-800 mb-5 text-center">
+          Real-Time Hand Tracking
+        </h1>
+        <div className="flex justify-center items-center w-full max-w-2xl rounded-lg overflow-hidden shadow-md bg-white">
+          <img
+            id="video-feed"
+            alt="Hand Tracking"
+            className="w-full h-auto object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Record;
